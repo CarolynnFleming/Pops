d26d0fd0a68b95bc8edaa29db3bee5e6ff2b1d73
CarolynnFleming <cfcatpink@gmail.com>
2023-03-20 18:50:55 -0400

    bagdata

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,41 @@
-import data from './data';
-import Offering from './components/Offering';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import OfferingScreen from './screens/OfferingScreen';
+
 function App() {
   return (
+    <BrowserRouter>
     <div className="grid-container">
     <header className="row">
-        <div>
-            <img className="popcorn"src="./public/popcorn.gif" alt="popcorn"/>
+        <div><a href="/">
+            <img className="popcorn"src="./public/popcorn.gif" alt="popcorn"/></a>
             <a href="/" className="brand">WHATS POPPIN</a>
         </div>
     
             
                   
                   <div className="follow">
+                  <a className="rainbow-4" href="/offeringscreen">Offerings</a>
                     <a className="rainbow-4" href="/cart">Cart</a>
             <a className="rainbow-4" href="/signIn">Sign In</a>
-                  
+            
               </div>
         
 
     </header>
     <main>
-      <div>
-        <div className="row center">
-          {
-            data.offering.map((offering) =>(
-              <Offering key={offering._id} offering={offering}></Offering>
-
-            ))
-          }
-            
-            </div>
-          </div>
+      <Routes>
+      <Route path="/offeringscreen" element={<OfferingScreen/>}></Route>
+      <Route path="/" element={<HomeScreen/>} exact></Route>
+      </Routes>
+      
     </main>
     <footer className="row center">
         All rights reserved
     </footer>
 </div>
+</BrowserRouter>
   );
 }
 
