f6fa677077cee8207ed020b3db35b2bc92a60e36
CarolynnFleming <cfcatpink@gmail.com>
2023-03-20 22:50:54 -0400

    I changed the footer of my website to give more room in the nav/header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import BagScreen from './screens/BagScreen';
 import popcorn from './assets/popcorn.gif';
 import MixesScreen from './screens/MixesScreen';
 import About from './screens/About';
+import ContactScreen from './screens/ContactScreen';
 function App() {
   return (
     <BrowserRouter>
@@ -19,7 +20,7 @@ function App() {
             
                   
                   <div className="follow">
-                  <a className="rainbow-4" href="/about">About Us</a>
+                  <a className="rainbow-4" href="/about">About</a>
                   <a className="rainbow-4" href="/offeringscreen">Offerings</a>
                   
                     <a className="rainbow-4" href="/cart">Cart</a>
@@ -32,6 +33,7 @@ function App() {
     <main>
       <Routes>
       <Route path="/about/" element={<About/>}></Route>
+      <Route path="/contactscreen/" element={<ContactScreen/>}></Route>
       <Route path="/offeringscreen/" element={<OfferingScreen/>}></Route>
       <Route path="/offering/1" element={<BagScreen/>} exact></Route>
       <Route path="/offering/3" element={<MixesScreen/>} exact></Route>
@@ -39,8 +41,10 @@ function App() {
       </Routes>
       
     </main>
-    <footer className="row center">
+    <footer className="bottom">
+    <a className="rainbow-4" href="/contactscreen">Contact</a>
         All rights reserved
+        <a className="rainbow-4" href="/about">About</a>
     </footer>
 </div>
 </BrowserRouter>
