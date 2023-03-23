c7f75b9872843012b133c99de325ba6eb5aea959
CarolynnFleming <cfcatpink@gmail.com>
2023-03-23 12:37:36 -0400

    created an express server  to make an api of my data

---
+++ backend/data.js
@@ -0,0 +1,77 @@
+const data = {
+    offerings: [
+        {
+            _id:'1',
+            name: 'Bags',
+            location:'/Bags',
+            image:'/public/bags.jpeg',
+            flavors: 4,
+            sizes: 4,  
+        },
+{
+    _id:'2',
+    name: 'Grab & Go',
+    location:'/Gng',
+    image:'/public/grabngo.jpg',
+    flavors: 10,
+    sizes: 1,  
+},
+{
+    _id:'3',
+    name: 'Mixes Bags',
+    location:'/mixesbags',
+    image:'/public/Mix.jpg',
+    flavors: 3,
+    sizes: 4,  
+},
+{
+    _id:'4',
+    name: 'Bucket',
+    location:'/bucket',
+    image:'/public/Bucket.jpg',
+    flavors: 7,
+    sizes: 1,  
+},
+{
+    _id:'5',
+    name: '1 Gallon',
+    location:'/oneg',
+    image:'/public/oneG.jpg',
+    flavors: 7,
+    sizes: 1,  
+},
+{
+    _id:'6',
+    name: '1 Gallon Sports Tin',
+    location:'sportstin',
+    image:'/public/sportsT.jpeg',
+    flavors: 7,
+    sizes: 1,  
+},
+{
+    _id:'7',
+    name: '2 Gallon Tin',
+    location:'/twog',
+    image:'/public/goldTin.jpeg',
+    flavors: 10,
+    sizes: 1,  
+},
+{
+    _id:'8',
+    name: '3 Gallon Tin',
+    location:'/threeg',
+    image:'/public/whiteTin.jpeg',
+    flavors: 7,
+    sizes: 1,  
+},
+{
+    _id:'9',
+    name: 'Italian Ice',
+    location:'/italianice',
+    image:'/public/iice.jpeg',
+    flavors: 12,
+    sizes: 4,  
+}
+]
+}
+export default data;
